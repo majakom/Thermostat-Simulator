b933fb88a2486abdb087b8e4fa8b0a542d24e10c
Maja Komorowska <majakom863@gmail.com>
2023-11-30 22:51:44 +0100

    Working on GetAllData and thermal conductivity in json file

--- thermostat-formulas.py
+++ thermostat-formulas.py
@@ -1,18 +1,30 @@
-TempAmb #ambient temperature const
-Tw = [] #water temperature
-P #power const
-t = [] #list to keep track of time
-tempMax = 90 #maks temperature to get from thermostat
-tempMin = 50 #min temperature to get from thermostat
-tempAmbMin = 7 #min temperature to use thermostat in
-tempAmbMax = 40 #maks temperature to use thermostat in
-timeMax = 3600 #maks time in which you can heat water
-timeMin = 120 #min time in which you can heat water
-timeWanted #time needed to heat water
-TempMax #temperature you want to achieve
+import json
+Power = [] #power const
+PowerMin #min power
+PowerMax #max power
+
+time = [] #list to keep track of time
+timeSimulation = 4000 #s - time of the simulation
 Ti = 0.1 #min time difference
+N = int(timeSimulation/Ti) + 1 #the amount of the tests
+
+TempMax = 90 #max temperature to get from thermostat
+TempMin = 50 #min temperature to get from thermostat
+TempAmbMin = 7 #min temperature to use thermostat in
+TempAmbMax = 40 #max temperature to use thermostat in
+TempAmb #ambient temperature - const
+TempWanted #temperature you want to achieve
+TempWater = [] #water temperature
+
+Kp = 0.02 #regulator gain
+
+
+U_min # min voltage
+U_max #max voltage
+
+
 qt = [] #heat provided by the heater
-q0 = [] #heat lost to the environment
+q0 = [] #heat loss to the environment
 h #fabric thickness
 lambd #thermal conductivity depending on the fabric
 m #mass of the fabric
@@ -22,19 +34,39 @@ I #current const
 
 
 
-def GetAllData(TempMax, tmax, Ta):
-    print("Time in which you want to heat water?\n")
-    while(timeMax<= timeWanted and timeWanted <= timeMin){
-        timeWanted = input()
+def GetAllData(TempAmbMin, TempAmbMax):
+    print("Enter the value of ambient temperature in Celcius (assuming it is a constant):")
+    TempAmb = float(input())
+    while(TempAmbMin>=TempAmb and TempAmb>=TempAmbMax){
+        if(TempAmbMin>=TempAmb){
+            print("Chosen temperature is too low. Min value = 7 C. Try again:")
+        } elif (TempAmb>=TempAmbMax) {
+            print("Chosen temperature is too high. Max value = 40 C. Try again:")
+        }
+        TempAmb = float(input())
     }
-    while(tempMin>=TempMax and TempMax>=TempMax){
-        print("Temperature you want: ")
-        TempMax = input()
+
+    print("Enter the value of wanted temperature of water in Celcius:")
+    TempWanted = float(input())  
+    while(TempMin>=TempWanted and TempWanted>=TempMax){
+        if(TempMin>=TempWanted){
+            print("Chosen temperature is too low. Min value = 7 C. Try again:")
+        } elif (TempWanted>=TempMax) {
+            print("Chosen temperature is too high. Max value = 40 C. Try again:")
+        }
+        TempWanted = float(input())
     }
-    while(tempAmbMin>=Ta and Ta>=tempAmbMax){
-        print("Temperature in your enivronment: ")
-        TempAmb = input()
-    }
+
+    print("Choose the material of the thermostat:")
+    print("(0) Stainless steel")
+    print("(1) Ceramic")
+    print("(2) Glass")
+    print("(3) Copper")
+    print("(4) Aluminium")
+    choice = int(input())
+    match choice:
+        case 0:
+            
 
 
     
