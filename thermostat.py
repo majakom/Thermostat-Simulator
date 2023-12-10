201f910b327ce636b20bc87a2fa44f2199f05a58
Maja Komorowska <majakom863@gmail.com>
2023-12-10 02:16:58 +0100

    Added requests

--- thermostat.py
+++ thermostat.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
-#import thermostat_formulas
+from flask import Flask, render_template, request, jsonify
+#import thermostat_formulas as main
 
 app = Flask(__name__, template_folder='html_files')
 @app.route("/")
 @app.route("/flaskApp")
 def flaskApp():
     return render_template('flaskApp.html')
+
+@app.route("/GetParameters", methods=["POST"])
+def GetParameters():
+    material = request.form.get("typeMaterial")
+    ambient = request.form.get("AmbientTemperature")
+    temperature = request.form.get("Temperature")
+
+    print(f"Material: {material}, Ambient: {ambient}, Temperature: {temperature}")
+    return "bruh"
 
 if __name__ == '__main__':  
    app.run(debug=True)
