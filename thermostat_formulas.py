f85a0f20fa4c99a92125a7e997f7a8213e39d3dc
Maja Komorowska <majakom863@gmail.com>
2024-01-12 15:01:59 +0100

    Modified parameters part two

--- thermostat_formulas.py
+++ thermostat_formulas.py
@@ -102,21 +102,57 @@ def Calculate(data):
     newKettle, TempWanted, TempAmb =  GetDataFromFlask(data)
 
     if (newKettle == materials[0]):
-        Kp = 0.0000005999 #regulator gain
-        timeI = 0.0005
-        timeD = 0.000001
+        if(TempWanted <= 60 and TempAmb >=29):
+            Kp = 0.000001
+            timeI = 0.0001
+            timeD = 0.000001
+        elif (TempWanted >= 80): 
+            Kp = 0.0002
+            timeI = 1
+            timeD = 3
+        else:
+            Kp = 0.0000005999 #regulator gain
+            timeI = 0.0005
+            timeD = 0.000001
     elif (newKettle == materials[1]):
-        Kp = 0.00000999999999 #regulator gain
-        timeI = 0.0099
-        timeD = 4    
+        if(TempWanted <= 60 and TempAmb >=29):
+            Kp = 2 #regulator gain
+            timeI = 0.01
+            timeD = 4    
+        elif (TempWanted >= 80): 
+            Kp = 0.00075 #regulator gain
+            timeI = 3
+            timeD = 4    
+        else:
+            Kp = 0.00000999999999 #regulator gain
+            timeI = 0.0099
+            timeD = 4    
     elif (newKettle == materials[2]):
-        Kp = 0.000105 #regulator gain
-        timeI = 0.00859
-        timeD = 3
+        if(TempWanted <= 60 and TempAmb >=29):
+            Kp = 1 #regulator gain
+            timeI = 0.1
+            timeD = 3
+        elif (TempWanted >= 61):
+            Kp = 0.003 #regulator gain
+            timeI = 1.5
+            timeD = 3           
+        else:
+            Kp = 0.000105 #regulator gain
+            timeI = 0.00859
+            timeD = 3
     elif (newKettle == materials[3]):
-        Kp = 0.01 #regulator gain
-        timeI = 1.5
-        timeD = 10
+        if(TempWanted <= 60 and TempAmb >=29):
+            Kp = 0.7 #regulator gain
+            timeI = 0.1
+            timeD = 3
+        elif (TempWanted >= 61):
+            Kp = 0.003 #regulator gain
+            timeI = 1.5
+            timeD = 10            
+        else:
+            Kp = 0.01 #regulator gain
+            timeI = 1.5
+            timeD = 10
 
     # GetAllData(TempAmbMin, TempAmbMax, TempMax, TempMin)
 
