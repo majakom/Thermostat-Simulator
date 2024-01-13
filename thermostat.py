757c00892694fbc51d759257b3b018a58d471921
Maja Komorowska <majakom863@gmail.com>
2024-01-13 16:03:35 +0100

    Added sliders, modified Calculate function

--- thermostat.py
+++ thermostat.py
@@ -17,17 +17,20 @@ def GetParameters():
     material = request.form.get("typeMaterial")
     ambient = request.form.get("AmbientTemperature")
     temperature = request.form.get("Temperature")
-    kp = request.form.get("Kp")
+    regulator_gain = request.form.get("regulatorGain")
     Ti = request.form.get("Ti")
     Td = request.form.get("Td")
     TInterval = request.form.get("TInterval")
-    data = [int(material), int(ambient), int(temperature), float(kp), float(Ti), float(Td), float(TInterval), int(regulator)]
+    data = [int(material), int(ambient), int(temperature), float(regulator_gain), float(Ti), float(Td), float(TInterval), int(regulator)]
     main.LoadJson()
-    print(f"Material: {material}, Ambient: {ambient}, Temperature: {temperature}")
+    print(f"Material: {material}, Ambient: {ambient}, Temperature: {temperature}, regulator_gain: {regulator_gain}, ")
 
     time, TempWater, Denisty, e, HeatOut, HeatIn, HeatSum, ThermalCapacity = main.Calculate(data)
     df = pd.DataFrame({'Time [s]':time, 'Water temperature [C]':TempWater})
-    fig =px.line(df, x='Time [s]', y='Water temperature [C]', title="Thermostat - PID regulator")
+    if(regulator == 0):
+        fig =px.line(df, x='Time [s]', y='Water temperature [C]', title="Thermostat - PID regulator")
+    else:
+        fig =px.line(df, x='Time [s]', y='Water temperature [C]', title="Thermostat - PI regulator")
     
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template("flaskApp.html", graphJSON=graphJSON)
